4c73182bbdf2d98f147b97e2dc94235ffc09c544
kavabuchi <mikha.buvalez@gmail.com>
2025-07-05 12:01:04 +0200

    add player coef

--- player.py
+++ player.py
@@ -1,22 +1,38 @@
 class Player:
-    def __init__(self, name, age, position, shooting, defense, bloking, price):
+
+    position_stats = {
+        "Point_Guard": {"speed": 0.8, "dribble": 0.7, "pass": 0.6, "shot": 0.9},
+        "Shooting_Guard": {"speed": 0.7, "dribble": 0.7 , "pass": 0.5, "shot": 0.8},
+        "Small_Forward": {"speed": 0.6, "dribble": 0.6, "pass": 0.6, "shot": 0.7}, 
+        "Power_Forward": {"speed": 0.5, "dribble": 0.5, "pass": 0.5, "shot": 0.6},
+        "Center": {"speed": 0.4, "dribble": 0.5, "pass": 0.4, "shot": 0.5}
+    }
+
+    def __init__(self, name, age, position, fatigue, skill, price):
         self.name = name
         self.age = age
         self.position = position
-        self.shooting = shooting
-        self.defense = defense
-        self.bloking = bloking
-        self.player_coef = (shooting * 0.4 + defense * 0.5 + bloking * 0.3) * price
+        self.fatigue = fatigue
+        self.skill = skill
         self.price = price
+        self.palyer_coef = 0
+        self.count_coef()
+    
+    def count_coef(self):
+        stats = self.position_stats[self.position]
+        skill_sum = 0
 
-
+        for key in stats:
+            skill_sum += stats[key] * self.skill
+        self.palyer_coef = skill_sum * (1 - self.fatigue)
+        
     def __str__(self):
-        return f"Player: {self.name}, Age: {self.age}, Position: {self.position}, Shooting: {self.shooting}, Defense: {self.defense}, Bloking: {self.bloking}, Price: {self.price}, Player Coefficient: {self.player_coef}"
+        return f"Player: {self.name}, Age: {self.age}, Position: {self.position}, Fatigue: {self.fatigue}, Skill: {self.skill}, Price: {self.price}, Coef: {self.palyer_coef}"
 
 
 if __name__ == "__main__":
-    player1 = Player("Koby Bryant", 25, 1, 0.8, 0.7, 0.9, 100000)
-    player2 = Player("Lebron James", 30, 2, 0.9, 0.8, 0.7, 90000)
-
+    player1 = Player("Koby Bryant", 25, "Point_Guard", 0.3, 90, 10000)
+    player2 = Player("Lebron James", 25, "Shooting_Guard", 0.4, 92, 15000)
+    
     print(player1)
     print(player2)
